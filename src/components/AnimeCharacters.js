f32e819cf76c95f83af93c93d453fa91c33da4b0
VijayKrishna549 <vijayk@bipp.io>
2023-04-21 19:46:36 +0530

    Removed unwanted comments and console logs, Added new comments for implemented functionalities in code

--- src/components/AnimeCharacters.js
+++ src/components/AnimeCharacters.js
@@ -13,17 +13,16 @@ function AnimeCharacters() {
 
   const inputHandler = (e) => {
     setInputText(e.target.value.toLowerCase());
-    console.log(e.target.value.toLowerCase());
   };
 
-  // handle next
+  // handle next click in pagination
 
   const handleNext = () => {
     if (page === pageCount) return page;
     setPage(page + 1);
   };
 
-  // handle previous
+  // handle previous clicj in pagination
 
   const handlePrev = () => {
     console.log("perv clicked");
@@ -40,7 +39,6 @@ function AnimeCharacters() {
       )
       .then((response) => {
         console.log(response.data.data, "response");
-        console.log(response.data.data[0].images.jpg.image_url, "response33");
         setAnimeChar(response.data.data);
       })
       .catch((error) => {
@@ -48,6 +46,7 @@ function AnimeCharacters() {
       });
   }, [page]);
 
+  // Assigning number of elements per page
   useEffect(() => {
     const pageDataCount = Math.ceil(animeChar.length / 4);
     setPageCount(pageDataCount);
@@ -61,7 +60,7 @@ function AnimeCharacters() {
   return (
     <>
       <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" class="form-label"></label>
+        <label htmlFor="exampleInputEmail1" className="form-label"></label>
         <input
           onChange={inputHandler}
           type="search"
@@ -72,66 +71,67 @@ function AnimeCharacters() {
       </div>
 
       <div className="on">
-        {pageData
-          .filter((element) => {
-            // console.log(element, "elele");
-            if (element === "") {
-              return element;
-            } else {
-              return (
-                element.name.toLowerCase().includes(inputText) ||
-                element.name_kanji.toLowerCase().includes(inputText)
-              );
-            }
-          })
-          .map((characters) => (
-            <div
-              className="card"
-              style={{ width: "300px" }}
-              key={characters.mal_id}
-            >
-              {/* <img src={p} className="card-img-top" alt="..."></img> */}
-              <img
-                src={characters.images.jpg.image_url}
-                className="card-img-top"
-                style={{ width: "300px", height: "250px" }}
-                alt="..."
-              ></img>
-              <div className="card-body">
-                <h5 className="card-title">Name: {characters.name}</h5>
-                <h5 className="card-title">
-                  Name_Kanji: {characters.name_kanji}
-                </h5>
-                <p
-                  className="card-text"
-                  style={{
-                    maxHeight: "200px",
-                    overflow: "auto",
-                  }}
-                >
-                  About Me: {characters.about}
-                </p>
-                <div class="fav">
-                  {characters.favorites}
-                  <i class="material-icons">favorite</i>
-                </div>
-                <div style={{ marginTop: "20px" }}>
-                  <a href={characters.url} class="btn btn-primary">
-                    <i class="material-icons" style={{ width: "200px" }}>
-                      arrow_forward
-                    </i>
-                  </a>
+        {
+          // Search for anime characters
+          pageData
+            .filter((element) => {
+              if (element === "") {
+                return element;
+              } else {
+                return (
+                  element.name.toLowerCase().includes(inputText) ||
+                  element.name_kanji.toLowerCase().includes(inputText)
+                );
+              }
+            })
+            .map((characters) => (
+              <div
+                className="card"
+                style={{ width: "300px" }}
+                key={characters.mal_id}
+              >
+                <img
+                  src={characters.images.jpg.image_url}
+                  className="card-img-top"
+                  style={{ width: "300px", height: "250px" }}
+                  alt="..."
+                ></img>
+                <div className="card-body">
+                  <h5 className="card-title">Name: {characters.name}</h5>
+                  <h5 className="card-title">
+                    Name_Kanji: {characters.name_kanji}
+                  </h5>
+                  <p
+                    className="card-text"
+                    style={{
+                      maxHeight: "200px",
+                      overflow: "auto",
+                    }}
+                  >
+                    About Me: {characters.about}
+                  </p>
+                  <div className="fav">
+                    {characters.favorites}
+                    <i className="material-icons">favorite</i>
+                  </div>
+                  <div style={{ marginTop: "20px" }}>
+                    <a href={characters.url} className="btn btn-primary">
+                      <i className="material-icons" style={{ width: "200px" }}>
+                        arrow_forward
+                      </i>
+                    </a>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))
+        }
       </div>
       <div className="d-flex justify-content-end">
         <nav aria-label="Page navigation example">
-          <ul class="pagination">
-            <li class="page-item">
+          <ul className="pagination">
+            <li className="page-item">
               <a
-                class="page-link"
+                className="page-link"
                 href="#"
                 onClick={handlePrev}
                 disabled={page === 1}
@@ -139,14 +139,15 @@ function AnimeCharacters() {
                 Previous
               </a>
             </li>
+            {/* Enabling pagination on numbers apart from "previous" and "next" button */}
             {Array(pageCount)
               .fill(null)
               .map((ele, index) => {
                 return (
                   <>
-                    <li class="page-item">
+                    <li className="page-item">
                       <a
-                        class="page-link"
+                        className="page-link"
                         href="#"
                         active={page === index + 1 ? true : false}
                         onClick={() => setPage(index + 1)}
@@ -158,9 +159,9 @@ function AnimeCharacters() {
                 );
               })}
 
-            <li class="page-item">
+            <li className="page-item">
               <a
-                class="page-link"
+                className="page-link"
                 href="#"
                 onClick={handleNext}
                 disabled={page === pageCount}
